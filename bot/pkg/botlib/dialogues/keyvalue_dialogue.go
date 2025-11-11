33d1ef13d686f027600bda7cbf3704cf52999a13
Andrey Chernykh <andreychernykh04@gmail.com>
2025-11-11 18:13:44 +0300

    #86: refactor dialogue handling to use topic keys and improve error messages

--- bot/pkg/botlib/dialogues/keyvalue_dialogue.go
+++ bot/pkg/botlib/dialogues/keyvalue_dialogue.go
@@ -8,30 +8,43 @@ import (
 )
 
 type keyValueDialogue struct {
-	key        string
 	dataSource keyvalue.Store
+	key        string
 }
 
 func (k keyValueDialogue) ChangeTopic(ctx context.Context, topic Topic) error {
-	err := k.dataSource.Write(ctx, k.key, string(topic))
+	err := k.dataSource.Write(ctx, k.topicKey(), string(topic))
 	if err != nil {
-		return fmt.Errorf("(%T->%T) changing topic for dialogue #%s: %w", k, k.dataSource, k.key, err)
+		return fmt.Errorf(
+			"(%T->%T) writing dialogue(%s) topic to %q: %w",
+			k, k.dataSource, k.key, topic, err,
+		)
 	}
 	return nil
 }
 
 func (k keyValueDialogue) Topic(ctx context.Context) (Topic, error) {
-	topic, err := k.dataSource.Read(ctx, k.key)
+	topic, err := k.dataSource.Read(ctx, k.topicKey())
 	if err != nil {
-		return TopicNeutral, fmt.Errorf("(%T->%T) getting topic for dialogue #%s: %w", k, k.dataSource, k.key, err)
+		return TopicNeutral, fmt.Errorf(
+			"(%T->%T) reading dialogue(%s) topic: %w",
+			k, k.dataSource, k.key, err,
+		)
 	}
 	return Topic(topic), nil
 }
 
 func (k keyValueDialogue) Exists(ctx context.Context) (bool, error) {
-	exists, err := k.dataSource.Exists(ctx, k.key)
+	exists, err := k.dataSource.Exists(ctx, k.topicKey())
 	if err != nil {
-		return false, fmt.Errorf("(%T->%T) checking existence for dialogue #%s: %w", k, k.dataSource, k.key, err)
+		return false, fmt.Errorf(
+			"(%T->%T) checking existence of dialogue(%s) topic: %w",
+			k, k.dataSource, k.key, err,
+		)
 	}
 	return exists, nil
+}
+
+func (k keyValueDialogue) topicKey() string {
+	return fmt.Sprintf("%s:topic", k.key)
 }
--- bot/pkg/botlib/dialogues/keyvalue_dialogues.go
+++ bot/pkg/botlib/dialogues/keyvalue_dialogues.go
@@ -1,7 +1,6 @@
 package dialogues
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/andreychh/coopera-bot/pkg/botlib/keyvalue"
@@ -11,26 +10,15 @@ type keyValueDialogues struct {
 	dataSource keyvalue.Store
 }
 
-func (k keyValueDialogues) StartDialogue(ctx context.Context, id int64, topic Topic) (Dialogue, error) {
-	err := k.dataSource.Write(ctx, k.key(id), string(topic))
-	if err != nil {
-		return nil, fmt.Errorf(
-			"(%T->%T) starting dialogue for chat id #%d with topic %s: %w",
-			k, k.dataSource, id, topic, err,
-		)
-	}
-	return k.Dialogue(id), nil
-}
-
 func (k keyValueDialogues) Dialogue(id int64) Dialogue {
 	return keyValueDialogue{
-		key:        k.key(id),
 		dataSource: k.dataSource,
+		key:        k.dialogueKey(id),
 	}
 }
 
-func (k keyValueDialogues) key(id int64) string {
-	return fmt.Sprintf("chat:%d:dialogue", id)
+func (k keyValueDialogues) dialogueKey(id int64) string {
+	return fmt.Sprintf("dialogue:%d", id)
 }
 
 func KeyValueDialogues(dataSource keyvalue.Store) Dialogues {
