08f75a2975b1b5951d98567f0e83c39181ca22da
Andrey Chernykh <andreychernykh04@gmail.com>
2025-11-11 12:36:21 +0300

    #86: refactor keyValueDialogue to use string key instead of int64 id

--- bot/pkg/botlib/dialogues/keyvalue_dialogue.go
+++ bot/pkg/botlib/dialogues/keyvalue_dialogue.go
@@ -8,34 +8,30 @@ import (
 )
 
 type keyValueDialogue struct {
-	id         int64
+	key        string
 	dataSource keyvalue.Store
 }
 
 func (k keyValueDialogue) ChangeTopic(ctx context.Context, topic Topic) error {
-	err := k.dataSource.Write(ctx, k.key(), string(topic))
+	err := k.dataSource.Write(ctx, k.key, string(topic))
 	if err != nil {
-		return fmt.Errorf("(%T->%T) writing topic for chat id #%d: %w", k, k.dataSource, k.id, err)
+		return fmt.Errorf("(%T->%T) changing topic for dialogue #%s: %w", k, k.dataSource, k.key, err)
 	}
 	return nil
 }
 
 func (k keyValueDialogue) Topic(ctx context.Context) (Topic, error) {
-	topic, err := k.dataSource.Read(ctx, k.key())
+	topic, err := k.dataSource.Read(ctx, k.key)
 	if err != nil {
-		return TopicNeutral, fmt.Errorf("(%T->%T) reading topic for chat id #%d: %w", k, k.dataSource, k.id, err)
+		return TopicNeutral, fmt.Errorf("(%T->%T) getting topic for dialogue #%s: %w", k, k.dataSource, k.key, err)
 	}
 	return Topic(topic), nil
 }
 
 func (k keyValueDialogue) Exists(ctx context.Context) (bool, error) {
-	exists, err := k.dataSource.Exists(ctx, k.key())
+	exists, err := k.dataSource.Exists(ctx, k.key)
 	if err != nil {
-		return false, fmt.Errorf("(%T->%T) checking existence for chat id #%d: %w", k, k.dataSource, k.id, err)
+		return false, fmt.Errorf("(%T->%T) checking existence for dialogue #%s: %w", k, k.dataSource, k.key, err)
 	}
 	return exists, nil
-}
-
-func (k keyValueDialogue) key() string {
-	return fmt.Sprintf("chat:%d:dialogue", k.id)
 }
