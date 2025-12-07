4b0862526d984713a8b345a1f42dcbb7f4ac3f6f
Andrey Chernykh <andreychernykh04@gmail.com>
2025-12-07 12:51:28 +0300

    Streamline actions using sources for improved structure and clarity

--- bot/internal/domain/actions/create_team_action.go
+++ bot/internal/domain/actions/create_team_action.go
@@ -5,36 +5,22 @@ import (
 	"fmt"
 
 	"github.com/andreychh/coopera-bot/internal/domain"
+	sources2 "github.com/andreychh/coopera-bot/internal/domain/sources"
 	"github.com/andreychh/coopera-bot/pkg/botlib/core"
 	"github.com/andreychh/coopera-bot/pkg/botlib/forms"
-	"github.com/andreychh/coopera-bot/pkg/botlib/updates/attrs"
-	telegram "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/andreychh/coopera-bot/pkg/botlib/sources"
 )
 
-type createTeamAction struct {
-	forms     forms.Forms
-	community domain.Community
-}
-
-func (c createTeamAction) Perform(ctx context.Context, update telegram.Update) error {
-	id, exists := attrs.ChatID(update).Value()
-	if !exists {
-		return fmt.Errorf("getting chat ID: chat ID not found")
-	}
-	name, err := c.forms.Form(id).Field("team_name").Value(ctx)
-	if err != nil {
-		return fmt.Errorf("getting team name from form for chat %d: %w", id, err)
-	}
-	_, err = c.community.UserWithTelegramID(id).CreateTeam(ctx, name)
-	if err != nil {
-		return fmt.Errorf("creating team for user with chat ID %d: %w", id, err)
-	}
-	return nil
-}
-
-func CreateTeam(forms forms.Forms, community domain.Community) core.Action {
-	return createTeamAction{
-		forms:     forms,
-		community: community,
-	}
+func CreateTeam(f forms.Forms, community domain.Community) core.Action {
+	return sources.Apply(
+		sources2.CurrentUser(community),
+		forms.CurrentValue(f, "team_name"),
+		func(ctx context.Context, user domain.User, name string) error {
+			_, err := user.CreateTeam(ctx, name)
+			if err != nil {
+				return fmt.Errorf("creating team %q: %w", name, err)
+			}
+			return nil
+		},
+	)
 }
